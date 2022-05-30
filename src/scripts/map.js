a6f73d090715b492bd4df4a4dbc9a96e49498ee3
Karol Binkowski <59345997+GrzywN@users.noreply.github.com>
2022-05-30 11:17:39 +0200

    add Map class, add footer section

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,23 +1,59 @@
-const markerCoordinates = [50.815347, 19.107427];
-const greenIcon = new L.Icon({
-  iconUrl:
-    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-const map = L.map('map', {
-  center: [...markerCoordinates],
-  zoom: 13,
-  zoomControl: false,
-  attributionControl: false,
-});
-const locationMarker = L.marker(markerCoordinates, { icon: greenIcon });
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
+class Map {
+  constructor(markerCoords) {
+    this.markerCoords = markerCoords;
+    this.popupMessage = popupMessage;
+  }
+
+  static createIcon() {
+    return new L.Icon({
+      iconUrl:
+        'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
+  }
+
+  init() {
+    this.icon = Map.createIcon();
+    this.leafletMap = this.createMap(this.coords);
+    this.addLayerToMap();
+    this.marker = this.createMarker();
+    this.addMarkerToMap;
+    this.bindPopup();
+  }
+
+  createMap() {
+    return L.map('map', {
+      center: [...this.markerCoords],
+      zoom: 13,
+      zoomControl: false,
+      attributionControl: false,
+    });
+  }
+
+  addLayerToMap() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-locationMarker.addTo(map);
-locationMarker.bindPopup('Pizzeria Karola');
+    }).addTo(this.leafletMap);
+  }
+
+  createMarker() {
+    return L.marker(markerCoordinates, { icon: greenIcon });
+  }
+
+  addMarkerToMap() {
+    locationMarker.addTo(this.leafletMap);
+  }
+
+  bindPopup() {
+    locationMarker.bindPopup(this.popupMessage);
+  }
+}
+
+const markerCoordinates = [50.815347, 19.107427];
+const popupMessage = 'Pizzeria Karola';
+const map = new Map(markerCoordinates, popupMessage);
